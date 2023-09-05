743f12b1ac90dba0394b36c462b123448e8b245f
283375 <bilibili283375@163.com>
2023-09-05 23:57:06 +0800

    chore: `SongIdSelector` layout

--- ui/designer/components/songIdSelector_ui.py
+++ ui/designer/components/songIdSelector_ui.py
@@ -15,49 +15,18 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QGroupBox, QHBoxLayout,
-    QLineEdit, QPushButton, QSizePolicy, QSpacerItem,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLineEdit,
+    QPushButton, QSizePolicy, QSpacerItem, QVBoxLayout,
+    QWidget)
 
 class Ui_SongIdSelector(object):
     def setupUi(self, SongIdSelector):
         if not SongIdSelector.objectName():
             SongIdSelector.setObjectName(u"SongIdSelector")
-        SongIdSelector.resize(470, 350)
+        SongIdSelector.resize(350, 250)
         SongIdSelector.setWindowTitle(u"SongIdSelector")
         self.verticalLayout_2 = QVBoxLayout(SongIdSelector)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.songIdSelectorQuickActionsGroupBox = QGroupBox(SongIdSelector)
-        self.songIdSelectorQuickActionsGroupBox.setObjectName(u"songIdSelectorQuickActionsGroupBox")
-        self.horizontalLayout = QHBoxLayout(self.songIdSelectorQuickActionsGroupBox)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.nextPackageButton = QPushButton(self.songIdSelectorQuickActionsGroupBox)
-        self.nextPackageButton.setObjectName(u"nextPackageButton")
-        self.nextPackageButton.setMinimumSize(QSize(100, 0))
-
-        self.horizontalLayout.addWidget(self.nextPackageButton)
-
-        self.nextSongIdButton = QPushButton(self.songIdSelectorQuickActionsGroupBox)
-        self.nextSongIdButton.setObjectName(u"nextSongIdButton")
-        self.nextSongIdButton.setMinimumSize(QSize(100, 0))
-
-        self.horizontalLayout.addWidget(self.nextSongIdButton)
-
-        self.previousSongIdButton = QPushButton(self.songIdSelectorQuickActionsGroupBox)
-        self.previousSongIdButton.setObjectName(u"previousSongIdButton")
-        self.previousSongIdButton.setMinimumSize(QSize(100, 0))
-
-        self.horizontalLayout.addWidget(self.previousSongIdButton)
-
-        self.previousPackageButton = QPushButton(self.songIdSelectorQuickActionsGroupBox)
-        self.previousPackageButton.setObjectName(u"previousPackageButton")
-        self.previousPackageButton.setMinimumSize(QSize(100, 0))
-
-        self.horizontalLayout.addWidget(self.previousPackageButton)
-
-
-        self.verticalLayout_2.addWidget(self.songIdSelectorQuickActionsGroupBox)
-
         self.searchLineEdit = QLineEdit(SongIdSelector)
         self.searchLineEdit.setObjectName(u"searchLineEdit")
         self.searchLineEdit.setFrame(True)
@@ -69,15 +38,56 @@ class Ui_SongIdSelector(object):
 
         self.verticalLayout_2.addItem(self.verticalSpacer)
 
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.previousPackageButton = QPushButton(SongIdSelector)
+        self.previousPackageButton.setObjectName(u"previousPackageButton")
+        self.previousPackageButton.setMinimumSize(QSize(75, 0))
+
+        self.horizontalLayout_2.addWidget(self.previousPackageButton)
+
         self.packComboBox = QComboBox(SongIdSelector)
         self.packComboBox.setObjectName(u"packComboBox")
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.packComboBox.sizePolicy().hasHeightForWidth())
+        self.packComboBox.setSizePolicy(sizePolicy)
 
-        self.verticalLayout_2.addWidget(self.packComboBox)
+        self.horizontalLayout_2.addWidget(self.packComboBox)
+
+        self.nextPackageButton = QPushButton(SongIdSelector)
+        self.nextPackageButton.setObjectName(u"nextPackageButton")
+        self.nextPackageButton.setMinimumSize(QSize(75, 0))
+
+        self.horizontalLayout_2.addWidget(self.nextPackageButton)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.previousSongIdButton = QPushButton(SongIdSelector)
+        self.previousSongIdButton.setObjectName(u"previousSongIdButton")
+        self.previousSongIdButton.setMinimumSize(QSize(75, 0))
+
+        self.horizontalLayout_3.addWidget(self.previousSongIdButton)
 
         self.songIdComboBox = QComboBox(SongIdSelector)
         self.songIdComboBox.setObjectName(u"songIdComboBox")
+        sizePolicy.setHeightForWidth(self.songIdComboBox.sizePolicy().hasHeightForWidth())
+        self.songIdComboBox.setSizePolicy(sizePolicy)
 
-        self.verticalLayout_2.addWidget(self.songIdComboBox)
+        self.horizontalLayout_3.addWidget(self.songIdComboBox)
+
+        self.nextSongIdButton = QPushButton(SongIdSelector)
+        self.nextSongIdButton.setObjectName(u"nextSongIdButton")
+        self.nextSongIdButton.setMinimumSize(QSize(75, 0))
+
+        self.horizontalLayout_3.addWidget(self.nextSongIdButton)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
 
 
         self.retranslateUi(SongIdSelector)
@@ -86,12 +96,11 @@ class Ui_SongIdSelector(object):
     # setupUi
 
     def retranslateUi(self, SongIdSelector):
-        self.songIdSelectorQuickActionsGroupBox.setTitle(QCoreApplication.translate("SongIdSelector", u"songIdSelector.quickActions", None))
-        self.nextPackageButton.setText(QCoreApplication.translate("SongIdSelector", u"songIdSelector.quickActions.nextPackageButton", None))
-        self.nextSongIdButton.setText(QCoreApplication.translate("SongIdSelector", u"songIdSelector.quickActions.nextSongIdButton", None))
-        self.previousSongIdButton.setText(QCoreApplication.translate("SongIdSelector", u"songIdSelector.quickActions.previousSongIdButton", None))
-        self.previousPackageButton.setText(QCoreApplication.translate("SongIdSelector", u"songIdSelector.quickActions.previousPackageButton", None))
         self.searchLineEdit.setPlaceholderText(QCoreApplication.translate("SongIdSelector", u"search.lineEdit.placeholder", None))
+        self.previousPackageButton.setText(QCoreApplication.translate("SongIdSelector", u"previous", None))
+        self.nextPackageButton.setText(QCoreApplication.translate("SongIdSelector", u"next", None))
+        self.previousSongIdButton.setText(QCoreApplication.translate("SongIdSelector", u"previous", None))
+        self.nextSongIdButton.setText(QCoreApplication.translate("SongIdSelector", u"next", None))
         pass
     # retranslateUi
 
