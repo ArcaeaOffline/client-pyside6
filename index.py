d918032b9ca6d6b128d6004b4f94cdbf2c9a33dd
283375 <bilibili283375@163.com>
2023-10-21 14:00:27 +0800

    impr: log uncaught exceptions

--- index.py
+++ index.py
@@ -23,6 +23,19 @@ rootLoggerFormatter = logging.Formatter(
 )
 
 
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    rootLogger.critical(
+        "Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback)
+    )
+
+
+sys.excepthook = handle_exception
+
+
 if __name__ == "__main__":
     QCoreApplication.setApplicationName("Arcaea Offline")
 
