8558f5e403b9f69c44ecbaa7599e20d8bd5742db
283375 <bilibili283375@163.com>
2023-10-23 16:18:35 +0800

    impr: handle exceptions in `TabOverview`

--- ui/implements/tabs/tabOverview.py
+++ ui/implements/tabs/tabOverview.py
@@ -1,3 +1,5 @@
+import logging
+
 from arcaea_offline.database import Database
 from PySide6.QtCore import QCoreApplication
 from PySide6.QtGui import QShowEvent
@@ -5,6 +7,8 @@ from PySide6.QtWidgets import QWidget
 
 from ui.designer.tabs.tabOverview_ui import Ui_TabOverview
 from ui.extends.shared.language import LanguageChangeEventFilter
+
+logger = logging.getLogger(__name__)
 
 
 class TabOverview(Ui_TabOverview, QWidget):
@@ -22,18 +26,27 @@ class TabOverview(Ui_TabOverview, QWidget):
         return super().showEvent(event)
 
     def updateOverview(self):
-        b30 = self.db.get_b30() or 0.00
-        self.b30Label.setText(str(f"{b30:.3f}"))
-        self.databaseDescribeLabel.setText(
-            self.describeFormatString.format(
-                self.db.count_packs(),
-                self.db.count_songs(),
-                self.db.count_difficulties(),
-                self.db.count_chart_infos(),
-                self.db.count_complete_chart_infos(),
-                self.db.count_scores(),
+        try:
+            b30 = self.db.get_b30() or 0.00
+            self.b30Label.setText(str(f"{b30:.3f}"))
+        except Exception:
+            logger.exception("Cannot get b30:")
+            self.b30Label.setText("ERR")
+
+        try:
+            self.databaseDescribeLabel.setText(
+                self.describeFormatString.format(
+                    self.db.count_packs(),
+                    self.db.count_songs(),
+                    self.db.count_difficulties(),
+                    self.db.count_chart_infos(),
+                    self.db.count_complete_chart_infos(),
+                    self.db.count_scores(),
+                )
             )
-        )
+        except Exception:
+            logger.exception("Cannot update overview:")
+            self.databaseDescribeLabel.setText("ERR")
 
     def retranslateUi(self, *args):
         super().retranslateUi(self)
